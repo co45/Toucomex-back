a93f258d1db8ac97a8ea160d4ec4b84c38b97070
co45 <chaabeneoussema1@gmail.com>
2021-06-18 04:18:31 +0200

    .

---
+++ src/main/java/com/Toucomex/Importation_Toucomex/Models/SuiviModel.java
@@ -0,0 +1,34 @@
+package com.Toucomex.Importation_Toucomex.Models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SuiviModel {
+
+    private String numdeclaration;
+    private String observation;
+    private String shipment;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate datearrivefp;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate datedeclaration;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate datearrivestock;
+    private String facture;
+    private String reception;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate datereception;
+    private String montantfacture;
+
+}
